282c4485bbb3209e3cfb287a21a9d088138d0ba1
AJuarez2023001 <HansAlexanderMerucry@gmail.com>
2023-02-18 13:27:31 -0600

    Segundo Ejercicio

---
+++ Condiciones.py
@@ -0,0 +1,18 @@
+# Un concunto de personas se hacerca a una fiesta de cumpleaños para resivir su refacciòn, 
+#la refacciòn se determina por medio de la edad menores a 18 años se le entregara pastel, mayores de edad seran tacos
+
+edad = int(input("Ingrese edad  "))
+
+if edad >18 :
+    print("Disfrute su pastel ")
+elif edad <7 :
+    print("Disfrute sus dulces")
+else:
+    print("Disfrute su taco ")
+
+
+
+
+    
+
+    
